f06d7cad06b218d640273bcfc3078601f858c096
spancha <spancha@ncsu.edu>
2013-10-20 12:12:00 -0400

    changing ack / data type to int

--- Project2/src/org/base/Constants.java
+++ Project2/src/org/base/Constants.java
@@ -1,7 +1,7 @@
 package org.base;
 
 public class Constants {
-	public static short AckPacket = (short) 43690;
-	public static short DataPacket = 21845;
+	public static int AckPacket =  43690;
+	public static int DataPacket = 21845;
 	public static String GROUP_IP = "203.0.113.0";
 }
