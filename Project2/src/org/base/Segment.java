90affce17444378d062efd47fae485ba5eb3ad3e
spancha <spancha@ncsu.edu>
2013-10-20 11:05:40 -0400

    added isLastSegment field

--- Project2/src/org/base/Segment.java
+++ Project2/src/org/base/Segment.java
@@ -1,5 +1,46 @@
 package org.base;
 
-public class Segment {
+import java.io.Serializable;
 
+public class Segment implements Serializable{
+	
+	private static final long serialVersionUID = 7830492147281088365L;
+	short ChecksumVal,type;
+	int  SequenceNumber;
+	byte[] data;
+	boolean isLastSegment = false;
+	
+	public synchronized short getType() {
+		return type;
+	}
+	public synchronized void setType(short type) {
+		this.type = type;
+	}
+	
+	
+	public boolean isLastSegment() {
+		return isLastSegment;
+	}
+	public void setLastSegment(boolean isLastSegment) {
+		this.isLastSegment = isLastSegment;
+	}
+	public synchronized short getChecksumVal() {
+		return ChecksumVal;
+	}
+	public synchronized void setChecksumVal(short checksumVal) {
+		ChecksumVal = checksumVal;
+	}
+	
+	public synchronized int getSequenceNumber() {
+		return SequenceNumber;
+	}
+	public synchronized void setSequenceNumber(int sequenceNumber) {
+		SequenceNumber = sequenceNumber;
+	}
+	public synchronized byte[] getData() {
+		return data;
+	}
+	public synchronized void setData(byte[] data) {
+		this.data = data;
+	}
 }
