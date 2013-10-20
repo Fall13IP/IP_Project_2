5a4d56b963b872f5935717771cff520730f5d466
Rituraj <Rituraj@192.168.2.16>
2013-10-20 11:04:31 -0700

    ConstantsClass+Segment

--- Project2/src/org/base/Segment.java
+++ Project2/src/org/base/Segment.java
@@ -1,5 +1,37 @@
 package org.base;
 
-public class Segment {
+import java.io.Serializable;
 
+public class Segment implements Serializable{
+	private static final long serialVersionUID = 7830492147281088365L;
+	short ChecksumVal,type;
+	int  SequenceNumber;
+	byte[] data;
+	public synchronized short getType() {
+		return type;
+	}
+	public synchronized void setType(short type) {
+		this.type = type;
+	}
+	
+	
+	public synchronized short getChecksumVal() {
+		return ChecksumVal;
+	}
+	public synchronized void setChecksumVal(short checksumVal) {
+		ChecksumVal = checksumVal;
+	}
+	
+	public synchronized int getSequenceNumber() {
+		return SequenceNumber;
+	}
+	public synchronized void setSequenceNumber(int sequenceNumber) {
+		SequenceNumber = sequenceNumber;
+	}
+	public synchronized byte[] getData() {
+		return data;
+	}
+	public synchronized void setData(byte[] data) {
+		this.data = data;
+	}
 }
