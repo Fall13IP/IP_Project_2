d3b8fcbbe1149f813a2fe2cb2af33d8d2d7ff5d6
spancha <spancha@ncsu.edu>
2013-10-20 12:45:06 -0400

    changed group ip address

--- Project2/src/org/base/Constants.java
+++ Project2/src/org/base/Constants.java
@@ -3,6 +3,6 @@ package org.base;
 public class Constants {
 	public static int AckPacket =  43690;
 	public static int DataPacket = 21845;
-	public static String GROUP_IP = "203.0.113.0";
+	public static String GROUP_IP = "225.0.113.0";
 	public static int MULTICAST_SOCKET = 4446;
 }
