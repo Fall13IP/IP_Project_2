fcf946c25a2ac2799a41242b94b11d1fcec356db
spancha <spancha@ncsu.edu>
2013-10-20 11:22:42 -0400

    added group ip for multicast

--- Project2/src/org/base/Constants.java
+++ Project2/src/org/base/Constants.java
@@ -3,5 +3,5 @@ package org.base;
 public class Constants {
 	public static short AckPacket = (short) 43690;
 	public static short DataPacket = 21845;
-
+	public static String GROUP_IP = "203.0.113.0";
 }
