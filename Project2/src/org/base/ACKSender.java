8a5b9b617ebb59944d1780196cc72fbc37b3405e
Rituraj <Rituraj@192.168.2.16>
2013-10-25 20:43:54 -0700

    ACKSender

--- Project2/src/org/base/ACKSender.java
+++ Project2/src/org/base/ACKSender.java
@@ -3,25 +3,17 @@ package org.base;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
-import java.net.Socket;
-import java.net.SocketException;
-import java.net.UnknownHostException;
 
-public class RDTSender extends Thread {
-	@Override
-	public void run() {
-		// TODO Auto-generated method stub
-		send();
-		
-	}
+public class ACKSender {
+
 	Segment segment;
 	String serverIP;
-	public RDTSender(Segment s, String ServerIP){
+	public ACKSender(Segment s, String ServerIP){
 		segment =s;
 		serverIP = ServerIP;
 		
 	}
-	private RDTSharedVariables rdtSharedVariables = RDTSharedVariables.getInstance();
+	
 	
 	public void send(){
 		DatagramSocket socket;
@@ -34,17 +26,13 @@ public class RDTSender extends Thread {
 
 		InetAddress address = InetAddress.getByName(serverIP);
 		InetAddress add2 = InetAddress.getLocalHost();
-		DatagramPacket packet = new DatagramPacket(buf, buf.length,address, Constants.SERVER_UDP_SOCKET);
+		DatagramPacket packet = new DatagramPacket(buf, buf.length,address, Constants.CLIENT_UDP_SOCKET);
         socket.send(packet);
-        RDTReceiver rdtReciever =new RDTReceiver();
-        rdtReciever.receive(Constants.CLIENT_UDP_SOCKET);
+        
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-	}
-	public static void SendOneClient() {
 		
 	}
 	
