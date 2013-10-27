ed64ea7ecf8006ca6e2bee07862ca6093852cc21
spancha <spancha@ncsu.edu>
2013-10-27 19:37:49 -0400

    updated socket numbers

--- Project2/src/org/base/Constants.java
+++ Project2/src/org/base/Constants.java
@@ -5,6 +5,6 @@ public class Constants {
 	public static int DataPacket = 21845;
 	public static String GROUP_IP = "225.0.113.0";
 	public static int MULTICAST_SOCKET = 4446;
-	public static int SERVER_UDP_SOCKET = 65505;
-	public static int CLIENT_UDP_SOCKET = 65550;
+	public static int SERVER_UDP_SOCKET = 65401;
+	public static int CLIENT_UDP_SOCKET = 65409;
 }
