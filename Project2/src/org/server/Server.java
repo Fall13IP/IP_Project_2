bbf97950c59b4349f51c07b70c4851272cef7382
spancha <spancha@ncsu.edu>
2013-10-27 11:35:36 -0400

    adding server functionality

--- Project2/src/org/server/Server.java
+++ Project2/src/org/server/Server.java
@@ -1,13 +1,88 @@
 package org.server;
 
+import java.io.ByteArrayOutputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.DatagramPacket;
+
+import java.util.Random;
+
+import org.base.ACKSender;
+import org.base.Constants;
+import org.base.RDTReceiver;
+
+import org.base.Segment;
+import org.base.SerializerDeserializer;
+
 public class Server {
 
 	public static void main(String[] args) {
 		
+		ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+		boolean lastSegmentReceived = false;
 		if(args.length == 3){
 			int portNo = Integer.parseInt(args[0]);
 			String filename = args[1];
-			int probability = Integer.parseInt(args[2]);
+			double probability = Double.parseDouble(args[2]);
+			Random random = new Random();
+			int expectedSequenceNumber = 1;
+			while(!lastSegmentReceived){
+				
+				DatagramPacket packet = RDTReceiver.receive(portNo);
+				double randValue = random.nextDouble();
+				if(randValue > probability)
+				{
+					Segment segment = SerializerDeserializer.deserialize(packet.getData());
+					if(segment.getType() == Constants.DataPacket){
+						
+						System.out.println(segment.getData().length);
+						if(segment.getSequenceNumber() == expectedSequenceNumber)
+						{
+							try {
+								byteArrayOutputStream.write(segment.getData());
+								Segment ack = new Segment();
+								ack.setSequenceNumber(expectedSequenceNumber);
+								ack.setType(Constants.AckPacket);
+								ACKSender ackSender = new ACKSender(ack, packet.getAddress().getHostAddress());
+								ackSender.send();
+								
+							
+								if(segment.isLastSegment() == true){
+									lastSegmentReceived = true;
+								}
+								expectedSequenceNumber++;
+							} catch (IOException e) {
+								// TODO Auto-generated catch block
+								e.printStackTrace();
+							}
+						}
+						else if(segment.getSequenceNumber() < expectedSequenceNumber){
+							Segment ack = new Segment();
+							ack.setSequenceNumber(segment.getSequenceNumber());
+							ack.setType(Constants.AckPacket);
+							ACKSender ackSender = new ACKSender(ack, packet.getAddress().getHostAddress());
+							ackSender.send();
+						}
+						
+					}
+				}
+				else
+					System.out.println("Packet loss simulated, value of rand: " + randValue);
+			}
+			System.out.println("Total file size: " + byteArrayOutputStream.size());
+			
+			try {
+				FileOutputStream fileOutputStream = new FileOutputStream(filename);
+				fileOutputStream.write(byteArrayOutputStream.toByteArray());
+				fileOutputStream.close();
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 			
 		}
 
