629ca7e356525320a51a06e3d53aa48e34231de4
spancha <spancha@ncsu.edu>
2013-10-27 14:38:43 -0400

    update client & server sockets

--- Project2/src/org/base/Constants.java
+++ Project2/src/org/base/Constants.java
@@ -5,6 +5,6 @@ public class Constants {
 	public static int DataPacket = 21845;
 	public static String GROUP_IP = "225.0.113.0";
 	public static int MULTICAST_SOCKET = 4446;
-	public static int SERVER_UDP_SOCKET = 60002;
-	public static int CLIENT_UDP_SOCKET = 60009;
+	public static int SERVER_UDP_SOCKET = 6000;
+	public static int CLIENT_UDP_SOCKET = 6001;
 }
